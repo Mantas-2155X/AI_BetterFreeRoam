0c2b8b43713f82c4a9b9ef81429bdcaa07e9ddf5
Mantas-2155X <syenergyy@gmail.com>
2020-05-04 13:24:16 +0300

    Disable map simulation

--- AI_BetterFreeRoam/AI_BetterFreeRoam.cs
+++ AI_BetterFreeRoam/AI_BetterFreeRoam.cs
@@ -4,6 +4,8 @@ using BepInEx.Configuration;
 
 using HarmonyLib;
 
+using Manager;
+
 using UnityEngine;
 
 namespace AI_BetterFreeRoam
@@ -14,14 +16,18 @@ namespace AI_BetterFreeRoam
         public const string VERSION = "1.0.0";
 
         private static Terrain terrain;
+        private static GameObject mapSimulation;
         private static bool inHousingIsland;
         
         private static ConfigEntry<bool> optimizeBaseMap { get; set; }
         private static ConfigEntry<bool> disableFoliage { get; set; }
+        private static ConfigEntry<bool> disableMapSimulation { get; set; }
+        
         private void Awake()
         {
             optimizeBaseMap = Config.Bind("Performance Improvements", "Optimize Basemap", true, new ConfigDescription("Optimize Basemap (only works in housing island)"));
             disableFoliage = Config.Bind("Performance Improvements", "Disable Foliage", false, new ConfigDescription("Disable Foliage"));
+            disableMapSimulation = Config.Bind("Performance Improvements", "Disable Map simulation", false, new ConfigDescription("Disable Map simulation (may disable some effects)"));
 
             optimizeBaseMap.SettingChanged += delegate
             {
@@ -39,12 +45,24 @@ namespace AI_BetterFreeRoam
                 terrain.drawTreesAndFoliage = !disableFoliage.Value;
             };
             
+            disableMapSimulation.SettingChanged += delegate
+            {
+                if (mapSimulation == null || HSceneManager.isHScene)
+                    return;
+
+                mapSimulation.SetActive(!disableMapSimulation.Value);
+            };
+
             HarmonyWrapper.PatchAll(typeof(AI_BetterFreeRoam));
         }
         
-        [HarmonyPostfix, HarmonyPatch(typeof(Manager.Map), "InitSearchActorTargetsAll")]
+        [HarmonyPostfix, HarmonyPatch(typeof(Map), "InitSearchActorTargetsAll")]
         public static void Map_InitSearchActorTargetsAll_Patch()
         {
+            mapSimulation = GameObject.Find("CommonSpace/MapRoot/MapSimulation(Clone)/EnviroSkyGroup(Clone)");
+            if (mapSimulation == null)
+                return;
+            
             var map = GameObject.Find("map00_Beach/map00_terrain_data/map00_terrain");
             if (map == null)
             {
@@ -58,11 +76,19 @@ namespace AI_BetterFreeRoam
             terrain = map.GetComponent<Terrain>();
             if (terrain == null)
                 return;
-            
+
             if(inHousingIsland)
                 terrain.basemapDistance = optimizeBaseMap.Value ? 0 : 1000;
             
             terrain.drawTreesAndFoliage = !disableFoliage.Value;
+            
+            mapSimulation.SetActive(!disableMapSimulation.Value);
+        }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(HSceneManager), "EndHScene")]
+        public static void HSceneManager_EndHScene_Patch()
+        {
+            mapSimulation.SetActive(!disableMapSimulation.Value);
         }
     }
 }
